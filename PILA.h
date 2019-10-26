c3c838a3def4a12d0b5753797d8517c354f99f50
LuisFC7 <luisitofc.28@gmail.com>
2019-10-25 23:55:03 -0500

    Se agregaron los prototipos de las funiones dentro de la libreria PILA.h

--- PILA.h
+++ PILA.h
@@ -1,13 +1,21 @@
 
 #define TAMMAX 200
 
-//Definicion del TDA "PILA"
+//Declaración  "PILA"
 typedef struct pila
 {
 	tipoDato listaPila[TAMMAX];
 	int cima;
 }PILA;
 
-//Operaciones fundamentales para el TDA "PILA"
-void crearPila(PILA *P);
 
+//Funciones de la Pila
+void crearPila(PILA *P);
+void insertarPila(PILA *P, tipoDato elemento);
+tipoDato quitarPila(PILA *P);
+void limpiarPila(PILA *P);
+
+tipoDato cimaPila(PILA P);
+
+int pilaLlena(PILA P);
+int pilaVacia(PILA P);
