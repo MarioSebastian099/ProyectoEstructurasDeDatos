c687416513a4cc8dd16d0ce2064929547dc5ba44
LuisFC7 <luisitofc.28@gmail.com>
2019-10-25 23:57:35 -0500

    Se realizaron las funciones insertar pila y quitar pila

--- funcionespila.c
+++ funcionespila.c
@@ -1,3 +1,4 @@
+typedef char tipoDato;
 #include "pila.h"
 
 //Función que inicializa el valor de pila en -1
@@ -6,3 +7,29 @@ void crearPila(PILA *P)
 	P->cima=-1;
 }
 
+//Ingreso de datos a la pila
+void insertarPila(PILA *P, tipoDato elemento)
+{
+	if(pilaLlena(*P)==1)
+	{
+		puts("Error la pila esta llena");
+		exit(-1);
+	}
+	P->cima++;
+	P->listaPila[P->cima]=elemento;
+}
+
+//pop elementos de pila
+tipoDato quitarPila(PILA *P)
+{
+	tipoDato aux;
+	
+	if(pilaVacia(*P)==1)
+	{
+		puts("Error la pila esta vacia");
+		exit(-1);
+	}
+	aux = P->listaPila[P->cima];
+	P->cima--;
+	return aux;
+}
