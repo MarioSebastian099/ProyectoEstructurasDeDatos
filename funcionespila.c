28cade860c171bc9a4bca073a2bcb2e6e16e380c
Israel Mendoza Rodriguez <ray200021@hotmail.com>
2019-10-30 17:19:51 -0600

    se realizaron las funciones LimpiarPila, PilaLlena y PilaVacia

--- funcionespila.c
+++ funcionespila.c
@@ -33,3 +33,71 @@ tipoDato quitarPila(PILA *P)
 	P->cima--;
 	return aux;
 }
+//Función que devuelve el valor de la cima a -1
+void limpiarPila(PILA *P)
+{
+	P->cima=-1;
+}
+
+//Funcion que lee el elemento de la cima de la pila sin elemininarlo
+tipoDato cimaPila(PILA P)
+{
+	return (P.listaPila[P.cima]);
+}
+
+//Funcion que determina si la pila está llena
+int pilaLlena(PILA P)
+{
+	if(P.cima==TAMMAX-1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+//Funcion que determina si la pila está vacia
+int pilaVacia(PILA P)
+{
+	if(P.cima==-1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;	
+	}
+	
+}
+
+
+char * cadenaPila(PILA P)
+{
+	PILA aux;
+	crearPila(&aux);
+
+	int i =0;
+
+	while(!pilaVacia(P))
+	{
+		insertarPila(&aux, quitarPila(&P));
+		i++;
+	}
+
+	char * cadena = (char*)malloc(i+1*sizeof(char));
+	
+	i=0;
+
+	while(!pilaVacia(aux))
+	{
+		cadena[i] = quitarPila(&aux);
+		i++;
+	}
+	cadena[i] = 0;
+
+	return cadena;
+}
+
+
