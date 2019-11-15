339228b2db439a4a8d15f7e22c94c8626cb3602d
Ambide <gunelite1999@gmail.com>
2019-11-15 17:38:19 -0600

    El archivo se modifico

---
+++ pila.h
@@ -0,0 +1,21 @@
+
+#define TAMMAX 200
+
+//Declaración  "PILA"
+typedef struct pila
+{
+	tipoDato listaPila[TAMMAX];
+	int cima;
+}PILA;
+
+
+//Funciones de la Pila
+void crearPila(PILA *P);
+void insertarPila(PILA *P, tipoDato elemento);
+tipoDato quitarPila(PILA *P);
+void limpiarPila(PILA *P);
+
+tipoDato cimaPila(PILA P);
+
+int pilaLlena(PILA P);
+int pilaVacia(PILA P);
