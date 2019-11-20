3573ebb9e268327878bc8c6bcfcd24c1f61005ce
Israel Mendoza Rodriguez <ray200021@hotmail.com>
2019-11-20 12:03:50 -0600

    se corrigieron lineas de la funcion recorridoarbol

--- codificador.c
+++ codificador.c
@@ -239,28 +239,29 @@ void recorridoAB(ArbolBinario raiz)
 
 {
 
-	if(raiz->izq)
-		{
-			insertarPila(&P, '0');
-			recorridoAB(raiz->izq);
-			quitarPila(&P);
-		}
-		
-		if(raiz->der)
-		{
-			insertarPila(&P, '1');
-			recorridoAB(raiz->der);
-			quitarPila(&P);
-		}
+        if(raiz->izq)
+                {
+                        insertarPila(&P, '0');
+                        recorridoAB(raiz->izq);
+                        quitarPila(&P);
+                }
+
+                if(raiz->der)
+                {
+                        insertarPila(&P, '1');
+                        recorridoAB(raiz->der);
+                        quitarPila(&P);
+                }
 
 
-	if((!raiz->izq) && (!raiz->der))
-	{
-		printf("%c = %s\n", raiz->dato.l,cadenaPila(P));
-		codigos[raiz->dato.l] = cadenaPila(P);
-		//printf("%s%dcoido\n", codigos[raiz->dato.l],5);
-	}
-	
-		
+        if((!raiz->izq) && (!raiz->der))
+        {
+                printf("%c = %s\n", raiz->dato.l,cadenaPila(P));
+                codigos[raiz->dato.l] = cadenaPila(P);
+                //printf("%s%dcoido\n", codigos[raiz->dato.l],5);
+        }
+
+
 }
+
 
